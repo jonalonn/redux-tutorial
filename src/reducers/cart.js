47819522e6d0d69b621100858a7201a6b48bd46c
jonalonn <jonathan.karnig@ffcg.se>
2018-12-05 10:43:39 +0100

    use actiontype constant

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,6 +1,8 @@
+import { ADD_ITEM_TO_CART } from "../actions/actionTypes";
+
 const cart = (state = [], action) => {
   switch (action.type) {
-    case "ADD_ITEM_TO_CART":
+    case ADD_ITEM_TO_CART:
       return [
         ...state,
         {
